4edc605d5da9af0300f7b9142f23f36f649b25fb
Victor Huisman <v.huisman@voiceintelligence.net>
2017-06-22 15:15:52 +0200

    minor change

--- Transsmart_api.php
+++ Transsmart_api.php
@@ -1,35 +1,43 @@
+/*
+  Class to test the creation of a shipment with Transsmart
+  Copyright Victor Huisman 2017
+ */
+
 <?php
 class Transsmart{
   var $user="transsmart@XXX.com";
   var $pass="PASSS";
   var $url="https://connect.test.api.transwise.eu/Api/";
-
+  
   function do_http_request($function,$type,$parameters,$postdata=null)
   {
+    /* send the API call to Transsmart
+     */
+
     if (count($parameters)>0)
       $workurl=$this->url.$function."?".http_build_query($parameters);
     else
       $workurl=$this->url.$function;
-
+    
     if ($type="post")
-    // use key 'http' even if you send the request to https://...
+      // use key 'http' even if you send the request to https://...
       $options = array(
-		     'http' => array(
-				     //'header'  => "Content-type: application/x-www-form-urlencoded\r\n"
-				     'header'  => "Authorization: Basic " . base64_encode($this->user.":".$this->pass),
-				     'method'  => 'POST',
-				     'content' => $postdata
+		       'http' => array(
+				       //'header'  => "Content-type: application/x-www-form-urlencoded\r\n"
+				       'header'  => "Authorization: Basic " . base64_encode($this->user.":".$this->pass),
+				       'method'  => 'POST',
+				       'content' => $postdata
 				     )
 		       );
     else
       $options = array(
-		     'http' => array(
-				     //'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-				     'header'=> "Authorization: Basic " . base64_encode($this->user.":".$this->pass),
-				     'method'  => 'GET',
-				     )
+		       'http' => array(
+				       //'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				       'header'=> "Authorization: Basic " . base64_encode($this->user.":".$this->pass),
+				       'method'  => 'GET',
+				       )
 		       );
-
+    
     $context  = stream_context_create($options);
     $result = file_get_contents($workurl, false, $context);
     var_dump($result);
@@ -38,12 +46,16 @@ class Transsmart{
     else
       return $result;
   } 
-
-
-
-
+  
+  
+  
+  
   function createDocument()
-  {
+  { 
+    /* a document is needed to send a shipment to transsmart.
+       this function sends a test document
+    */
+
     $post='{
     "Reference": "1",
     "AddressName": "Mr. Test",
@@ -83,11 +95,11 @@ class Transsmart{
       }
     ]
 }';
-
+    
     echo $this->do_http_request("Document","POST",array('autobook'=>'0','autolabel'=>'0','label_user'=>'','qzhost'=>'','zplprinter'=>'','laserprinter'=>'' ),$post); 
     
   }
-
+  
 }
 
 $t=new Transsmart();
